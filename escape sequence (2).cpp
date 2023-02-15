9eef651dc931cdb9f228f0d95769260fb6705a2e
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-15 09:04:18 +0500

    Add files via upload

---
+++ escape sequence (2).cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ string poem;
+poem="Hardworking makes a man successful,\n Life is short but not difficult,\n Everyone tries their hard to met the difficulties of life.\b";
+cout<<poem;
+    return 0;
+}
